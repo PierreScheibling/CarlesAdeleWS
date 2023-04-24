941ab5918538c87d2b8050fd0caf0437beef11eb
Pierre Scheibling <pierrescheibling@MacBook-Pro-de-Pierre.local>
2023-04-24 16:23:27 +0200

    change name

--- pages/_app.js
+++ pages/_app.js
@@ -30,7 +30,7 @@ export default function App({ Component, pageProps, router }) {
   return (
     <>
       <Head>
-        <title>Carles Avocat</title>
+        <title>CARLES Avocat</title>
         <meta name="description" content="Site Web du cabinet Carles Avocat" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
